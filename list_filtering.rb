17026e5d5e237155369d194903f831c76b1641ea
Robert Rosiji-Griffith <bibbycodes@gmail.com>
2019-09-21 16:02:12 +0100

    refactors list_filtering.rb

--- list_filtering.rb
+++ list_filtering.rb
@@ -1,9 +1,6 @@
 #In this kata you will create a function that takes a 
 #list of non-negative integers and strings and returns a
 #new list with the strings filtered out.
-
-list = [1,2,'a','b']
-
 def filter_list(l)
     filtered_list = []
     l.each do |item|
